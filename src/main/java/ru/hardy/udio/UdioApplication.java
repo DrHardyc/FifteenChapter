e123d36d048ac6bce0f4c346017dfd54067aca70
cherchesov <cherchesov@omsalania.ru>
2022-12-05 18:23:44 +0300

    first

---
+++ src/main/java/ru/hardy/udio/UdioApplication.java
@@ -0,0 +1,13 @@
+package ru.hardy.udio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdioApplication.class, args);
+    }
+
+}
